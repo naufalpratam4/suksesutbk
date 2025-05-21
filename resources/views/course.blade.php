cc89e694e7457523f4f836b41e28bf5ee75b5b08
unknown <naufal.d.pratama@gmail.com>
2025-05-21 14:29:28 +0700

    u

---
+++ resources/views/course.blade.php
@@ -0,0 +1,40 @@
+<div class="max-w-screen-lg mx-auto grid grid-cols-3 gap-4 pt-10">
+    <div class="col">
+        <div class="max-w-xl bg-white border border-gray-200 rounded-lg shadow-sm ">
+            <a href="#">
+                <img class="rounded-t-lg"
+                    src="https://www.lifewire.com/thmb/T7a6QlzUPEpua3OKx7V_pCvBY14=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/forest-wallpaper-af78609605b14f7c9b999d8b2f49c0d9.jpg"
+                    alt="" />
+            </a>
+            <div class="p-5">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 ">BUNDLING 3 TO SKD 2025 (PART 11, 12,
+                    13)</h5>
+                <p class="mb-3 font-normal ">Berisi 3 Paket Try Out dengan soal HOTS sesuai kisi-kisi SKD</p>
+
+                <hr class="mb-4 text-gray-400">
+
+                <div class="grid grid-cols-2 items-center mb-3">
+                    <div class="flex items-center">
+                        <span class="px-3 py-1 bg-green-700 rounded-lg text-white me-3">55%</span>
+                        <s class="text-gray-500">Rp 60.000</s>
+                    </div>
+                    <div class="text-right font-semibold text-black">Rp 20.000</div>
+                </div>
+
+
+                <a href="/detail"
+                    class="w-full inline-flex items-center justify-center px-4 py-3 text-sm font-medium text-center text-white bg-blue-700 rounded-xl hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
+                    Selengkapnya
+                    <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                        fill="none" viewBox="0 0 14 10">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                            d="M1 5h12m0 0L9 1m4 4L9 9" />
+                    </svg>
+                </a>
+            </div>
+        </div>
+
+    </div>
+    <div class="col bg-red-500">coba</div>
+    <div class="col bg-red-500">coba</div>
+</div>
